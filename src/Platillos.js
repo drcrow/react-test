4e2438a4eee4fdc3e3ef9c719aea796aff0ced11
Agustin Fiori <drcrow@gmail.com>
2017-05-11 13:57:11 -0300

    Platillos
    Agregada la lista de platillos con imagen, titulo y descripcion

--- src/Platillos.js
+++ src/Platillos.js
@@ -6,11 +6,28 @@ import VarTiendaController from './tiendaController';
 class Platillos extends Component{
 
 	render(){
+
+		let platillos_div = [];
+		const llenar_array_platillos_div = VarTiendaController.platillos.forEach(
+				(value, index)=>(
+						platillos_div.push(
+							<div key={index} className="list-group-item">
+								<div className="panel-body">
+									<img role="presentation" src={logo} className="ImagenPlatillo"/>
+									<h2 className="TitlePlatillo">{value.nombre}</h2>
+									<div className="DescripcionPlatillo">{value.descripcion}</div>
+								</div>
+							</div>
+						)
+					)
+			);
+
+
 		return(
 			<div className="container col-md-6">
 				<div>
 					<div className="panel panel-primary">
-						<div className="list-group Platillo-Menu"></div>
+						<div className="list-group Platillo-Menu">{platillos_div}</div>
 					</div>
 				</div>
 			</div>
